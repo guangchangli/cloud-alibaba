d2b16db0eea060fc2ed7f7d1df2cbda653ade0e0
guangchangli <guangchangde@gmail.com>
2020-02-25 00:34:57 +0800

    delete useless

--- cloud-alibaba-nacos-provider/src/main/java/com/aladdin/cloud/alibaba/nacos/provider/NacosProviderApplication.java
+++ cloud-alibaba-nacos-provider/src/main/java/com/aladdin/cloud/alibaba/nacos/provider/NacosProviderApplication.java
@@ -1,8 +1,12 @@
 package com.aladdin.cloud.alibaba.nacos.provider;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author lgc
@@ -13,8 +17,20 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class NacosProviderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NacosProviderApplication.class,args);
+        SpringApplication.run(NacosProviderApplication.class, args);
     }
 
+    @Value("${server.port}")
+    private String port;
+    @Value("${spring.application.name}")
+    private String appName;
 
+    @RestController
+    public class EchoController {
+        @GetMapping(value = "/echo/{message}")
+        public String echo(@PathVariable(value = "message") String message) {
+            return "Hello Nacos Discovery " + message + " i am from port " + port;
+//            return "hello nacos "+message;
+        }
+    }
 }
