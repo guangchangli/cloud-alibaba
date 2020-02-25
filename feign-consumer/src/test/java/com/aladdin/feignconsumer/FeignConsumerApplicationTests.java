2c74ff150968532cd07267aa97967fb5a3d7175b
guangchangli <guangchangde@gmail.com>
2020-02-25 12:28:29 +0800

    '改为 SpringInitializr'

---
+++ feign-consumer/src/test/java/com/aladdin/feignconsumer/FeignConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aladdin.feignconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
