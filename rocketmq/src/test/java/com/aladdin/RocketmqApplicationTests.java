0c26239c7bcea126d9f5a2b460927fffcbae1049
guangchang <guangchangde@gmail.com>
2020-08-21 23:43:30 +0800

    add

---
+++ rocketmq/src/test/java/com/aladdin/RocketmqApplicationTests.java
@@ -0,0 +1,15 @@
+package com.aladdin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest(classes = RocketmqApplication.class)
+@RunWith(SpringRunner.class)
+class RocketmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
